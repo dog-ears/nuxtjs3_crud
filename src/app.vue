30808c86f162cbefb1a997f611848030a0888d2d
dog-ears <info@dog-ears.net>
2024-05-18 07:07:54 +0900

    デフォルトレイアウトの適用

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <div class="app">トップページ</div>
+  </NuxtLayout>
 </template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="layouts-default">
+    <p>レイアウト：default</p>
+    <slot />
+  </div>
+</template>
