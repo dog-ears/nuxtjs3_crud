3637dcc05804a2c2438b01043413f67eb87208bb
dog-ears <info@dog-ears.net>
2024-05-18 07:12:52 +0900

    VercelのPage Speed Insight導入

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+</script>
+
 <template>
+  <SpeedInsights />
   <div class="layouts-default">
     <p>レイアウト：default</p>
     <slot />
